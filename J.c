5a0f1d27a06934f0e25f9eb6072e1d99bd50a59b
joanracheld <34539448+joanracheld@users.noreply.github.com>
2017-12-17 13:19:03 +0530

    Create J.c

---
+++ J.c
@@ -0,0 +1,19 @@
+# include<stdio.h>
+# include<conio.h>
+Void  main
+{ 
+int n;
+Scanf("%d",&n);
+if( n<0)
+{ 
+printf( "positive");
+}
+elseif(n=0)
+{ 
+printf("zero");
+} 
+elseif(n>0)
+{ 
+printf( "negative");
+} 
+} 
